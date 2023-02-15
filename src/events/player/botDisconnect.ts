eeeb3e66edc56d4c4e9db78b1ee582b0eb945925
eyerrock <timk.dreyer@web.de>
2023-02-15 21:48:17 +0100

    feat: botDisconnect event

---
+++ src/events/player/botDisconnect.ts
@@ -0,0 +1,27 @@
+import { Player, Queue } from "discord-player";
+import { Colors } from "discord.js";
+
+import { MetadataGuard } from "../../guards/MetadataGuard.js";
+import { PlayerEvent } from "../../types/Event.js";
+import { EmbedFactory } from "../../utils/EmbedFactory.js";
+
+export default {
+  name: "botDisconnect",
+  once: false,
+  execute: async (player: Player, queue: Queue) => {
+    if (!MetadataGuard.guardMetadata(queue))
+      return console.log("Queue has no valid metadata!");
+
+    const embed = new EmbedFactory()
+      .setColor(Colors.Red)
+      .setTitle("🛑 | Disconnected!")
+      .setDescription("I was disconnected from the voice channel.")
+      .setFooter({
+        text: "See you soon!",
+        iconURL: player.client.user?.displayAvatarURL(),
+      })
+      .create();
+
+    await queue.metadata.channel.send({ embeds: [embed] });
+  },
+} as PlayerEvent;
