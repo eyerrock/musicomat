81419fd43358014928c845e006381e79a96d20ff
Tim Dreyer <timk.dreyer@web.de>
2023-02-16 15:07:33 +0100

    refactor: default values for delays

--- src/database/schemas/GuildSchema.ts
+++ src/database/schemas/GuildSchema.ts
@@ -6,8 +6,8 @@ const GuildSchema = new Schema(
   {
     guildId: { type: String, required: true, unique: true },
     volume: { type: Number, required: true, default: 100, min: 0, max: 100 },
-    onEndDelay: { type: Number, required: true, default: 0, min: 0 },
-    onEmptyDelay: { type: Number, required: true, default: 0, min: 0 },
+    onEndDelay: { type: Number, required: true, default: 60, min: 0 },
+    onEmptyDelay: { type: Number, required: true, default: 60, min: 0 },
   },
   {
     timestamps: true,
