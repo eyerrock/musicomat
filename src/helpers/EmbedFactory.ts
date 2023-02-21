d5afd042d6227dc37d14ca27a96a5a75071e0e77
Tim Dreyer <timk.dreyer@web.de>
2023-02-21 14:24:54 +0100

    fix: timestamps on embeds

--- src/helpers/EmbedFactory.ts
+++ src/helpers/EmbedFactory.ts
@@ -1,6 +1,11 @@
 import { EmbedBuilder, EmbedFooterOptions, GuildMember } from "discord.js";
 
 export class EmbedFactory extends EmbedBuilder {
+  constructor() {
+    super();
+    this.setTimestamp();
+  }
+
   public setMemberFooter(member: GuildMember): this {
     return this.setFooter({
       text: `requested by ${member.displayName}`,
