026e6ac153d63e0b3be492f3ab632d4e9152284d
Tim Dreyer <timk.dreyer@web.de>
2023-02-28 14:07:07 +0100

    feat: watch command

---
+++ src/commands/Watch.ts
@@ -0,0 +1,93 @@
+import { Colors, SlashCommandBuilder } from "discord.js";
+
+import { EmbedFactory } from "../helpers/EmbedFactory.js";
+import { W2GAPIResponse } from "../types/API.js";
+import { Command } from "../types/Command.js";
+import { validateURL } from "../utils/validateURL.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("watch")
+    .setDescription("Creates a W2G-Room for the given URL.")
+    .addStringOption((option) =>
+      option
+        .setName("url")
+        .setDescription("The URL of the video to watch.")
+        .setRequired(true)
+    )
+    .setDMPermission(false),
+  aliases: ["w2g", "w"],
+  execute: async (interaction) => {
+    const url = interaction.options.getString("url") ?? "";
+    if (!validateURL(url)) {
+      const embed = new EmbedFactory()
+        .setColor(Colors.Red)
+        .setTitle("❌ ┃ Invalid URL")
+        .setDescription(
+          "The given URL is not valid!\nPlease try again with a valid URL."
+        )
+        .setMemberFooter(interaction.member);
+
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    await interaction.deferReply();
+
+    let data = null;
+    let res = null;
+    try {
+      res = await fetch("https://api.w2g.tv/rooms/create.json", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          w2g_api_key: process.env.W2G_API_KEY,
+          share: url,
+          bg_color: "#2F2F2F",
+          bg_opacity: "100",
+        }),
+      });
+
+      data = (await res.json()) as W2GAPIResponse;
+    } catch (err) {
+      console.log(err);
+      data = null;
+    }
+
+    if (!res?.ok || !data) {
+      const embed = new EmbedFactory()
+        .setColor(Colors.Red)
+        .setTitle("❌ ┃ Error")
+        .setDescription("An error occured while creating the room!")
+        .addFields(
+          {
+            name: "Status Code",
+            value: res?.status.toString() ?? "Unknown",
+            inline: true,
+          },
+          {
+            name: "Status Text",
+            value: res?.statusText ?? "Unknown",
+            inline: true,
+          }
+        )
+        .setMemberFooter(interaction.member);
+
+      await interaction.followUp({ embeds: [embed] });
+      return;
+    }
+
+    const embed = new EmbedFactory()
+      .setColor(Colors.Aqua)
+      .setTitle("📺 ┃ Watch Together")
+      .setDescription(
+        `Click [**here**](https://w2g.tv/rooms/${data.streamkey}) to join the room!`
+      )
+      .setMemberFooter(interaction.member);
+
+    await interaction.followUp({ embeds: [embed] });
+  },
+} as Command;
--- src/types/API.ts
+++ src/types/API.ts
@@ -10,3 +10,28 @@ export type APICommandRegistrationResponse = {
   description: string;
   dm_permission: boolean;
 };
+
+export type W2GAPIResponse = {
+  id: number;
+  streamkey: string;
+  created_at: string;
+  persistent: boolean;
+  persistent_name: string;
+  deleted: boolean;
+  moderated: boolean;
+  location: string;
+  stream_created: boolean;
+  background: string;
+  moderated_background: boolean;
+  moderated_playlist: boolean;
+  bg_color: string;
+  bg_opacity: number;
+  moderated_item: boolean;
+  theme_bg: string;
+  playlist_id: number;
+  members_only: boolean;
+  moderated_suggestions: boolean;
+  moderated_chat: boolean;
+  moderated_user: boolean;
+  moderated_cam: boolean;
+};
