a09b2b2a10be139c97dbe1837b52a4de152292b0
Tim Dreyer <timk.dreyer@web.de>
2023-02-28 12:04:27 +0100

    feat: stop command

---
+++ src/commands/Stop.ts
@@ -0,0 +1,30 @@
+import { Colors, SlashCommandBuilder } from "discord.js";
+
+import { EmbedFactory } from "../helpers/EmbedFactory.js";
+import { QueueController } from "../helpers/QueueController.js";
+import { Command } from "../types/Command.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("stop")
+    .setDescription("Stops the current Queue.")
+    .setDMPermission(false),
+  aliases: ["dc", "disconnect"],
+  execute: async (interaction, guild, client) => {
+    await interaction.deferReply();
+
+    const queue = await QueueController.getQueueByGuild(client, interaction);
+    if (!queue) return;
+
+    queue.destroy(false);
+    queue.connection.disconnect();
+
+    const embed = new EmbedFactory()
+      .setColor(Colors.LuminousVividPink)
+      .setTitle("💥 ┃ **Queue destroyed**")
+      .setDescription(`Queue has been destroyed!`)
+      .setMemberFooter(interaction.member);
+
+    await interaction.followUp({ embeds: [embed] });
+  },
+} as Command;
